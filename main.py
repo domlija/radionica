73c497e7d297fd513dd27b9351f6e8cb2689aad0
domlija <domlija@axiros.com>
2023-08-24 08:01:11 +0200

    s

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def search_user(username: str = ''):
 
 
 @app.get('/signup')
-async def get_signup_page(request: Request):
+async def get_signup_page(request: Request): #hh
     
 
 @app.post('/signup')
