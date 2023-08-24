0ab841b0d203c4129aed8270cd9697d82e7fd1f8
domlija <domlija@axiros.com>
2023-08-24 07:59:22 +0200

    first commit

--- main.py
+++ main.py
@@ -16,34 +16,20 @@ app.include_router(comments.router)
 
 @app.get("/")
 async def root(req: Request):
-    user_logged = False
-    if req.cookies.get('user_id'):
-        user_logged = True 
-
-
-    return templates.TemplateResponse('landing.view.html', {"request": req, "logged": user_logged})
+    
 
 @app.get("/search")
 async def search_user(username: str = ''):
-    print(username)
-    if username == '':
-        return RedirectResponse("/")
-    return RedirectResponse("/users/" + username, status_code=302)
+    
 
 
 @app.get('/signup')
 async def get_signup_page(request: Request):
-    logged = True if request.cookies.get('user_id') else False 
-    return templates.TemplateResponse("signup.view.html", context={"request": request, "logged": logged})
+    
 
 @app.post('/signup')
 async def signup_user(username: Annotated[str, Form()], password: Annotated[str, Form()], passwordRepeat: Annotated[str, Form()]):
-    try:    
-        User.fetch_by_username(username)
-        return {"status": "username exists"}
-    except:
-        User.insert_user(username, password)
-        return {"status": "Sign up successful"}
+
 
 @app.get('/login')
 async def get_login_page(request: Request):
@@ -52,13 +38,7 @@ async def get_login_page(request: Request):
 
 @app.post('/login')
 async def login_user(username: Annotated[str, Form()], password:Annotated[str, Form()]):
-    user_id = User.login_user(username, password)
-    if user_id == None:
-        return JSONResponse({"message": "failure"})
-    
-    res = RedirectResponse('/users/' + username, status_code=302)
-    res.set_cookie(key="user_id", value=str(user_id), secure=True, httponly=True)
-    return res
+
 
 @app.get('/logout')
 async def logout_user(req: Request):
--- routers/posts.py
+++ routers/posts.py
@@ -20,35 +20,16 @@ async def get_create_post(req: Request):
     if not logged:
         return {"status": "not logged in!"}
 
-    return templates.TemplateResponse("createPost.view.html", context={"request": req,
-                                                                       "logged": logged})
+    
 
 @router.get('/{post_id}')
 async def get_post(post_id, request: Request):
-    post = BlogPost.fetch_by_id(post_id)
-    comments = Comment.fetch_by_post_id(post_id)
-    logged = True if request.cookies.get('user_id') else False
-    #return post
-    return templates.TemplateResponse("blog.view.html", {"post_id": post_id, 
-                                                         "title": post.title, 
-                                                         "body": post.body, 
-                                                         "request": request, 
-                                                         "comments": comments,
-                                                         "logged": logged})
+    
 
 
 @router.post('/')
 async def put_post(title: Annotated[str, Form()], body: Annotated[str, Form()], request: Request):
-    user_id = request.cookies.get("user_id")
-    if user_id == None:
-        return {"status": "failed"}
     
-    user = User.fetch_by_id(user_id)
-
-
-    BlogPost.insert_post(title, body, user_id)
-
-    return RedirectResponse("/users/" + user.username, status_code=302)
 
 @router.delete('/{post_id}')
 async def delete_post(post_id, req: Request):
